7d0161dda9b0fb1df595a4fd411408266e8d4184
Q Z <13253467705@163.com>
2023-10-19 13:59:50 +0800

    1、bug fix

--- Sources/NNToast/NNToast.swift
+++ Sources/NNToast/NNToast.swift
@@ -11,11 +11,11 @@ import Toast
 
 open class Toast: NSObject {
     
-    static let `default`: Toast = {
+   public static let `default`: Toast = {
         return Toast()
     }()
     
-    static let autoDismissTime: TimeInterval = 2
+    public static let autoDismissTime: TimeInterval = 2
     
     public func showError(_ message: String?, clearTime: TimeInterval = 2) {
         if let message = message {
@@ -43,7 +43,7 @@ open class Toast: NSObject {
         Toast.clear()
     }
     
-    static func showToast(_ view: UIView, message: String, image: UIImage?, clearTime: TimeInterval = autoDismissTime, completion: ((_ didTap: Bool)->())? = nil) {
+    public static func showToast(_ view: UIView, message: String, image: UIImage?, clearTime: TimeInterval = autoDismissTime, completion: ((_ didTap: Bool)->())? = nil) {
         DispatchQueue.main.async {
             var style = ToastStyle()
             style.messageFont = UIFont.systemFont(ofSize: 15, weight: .regular)
@@ -55,46 +55,46 @@ open class Toast: NSObject {
         }
     }
     
-    static func showToast(_ message: String, image: UIImage?, clearTime: TimeInterval = autoDismissTime, completion: ((_ didTap: Bool)->())? = nil) {
+    public static func showToast(_ message: String, image: UIImage?, clearTime: TimeInterval = autoDismissTime, completion: ((_ didTap: Bool)->())? = nil) {
         DispatchQueue.main.async {
             guard let view = (UIApplication.shared.nn_keyWindow ?? self.topmostViewController?.view) else { return }
             self.showToast(view, message: message, image: image, clearTime: clearTime, completion: completion)
         }
     }
     
-    static func showSuccess(_ message: String, clearTime: TimeInterval = autoDismissTime,  completion: ((_ didTap: Bool)->())? = nil) {
+    public static func showSuccess(_ message: String, clearTime: TimeInterval = autoDismissTime,  completion: ((_ didTap: Bool)->())? = nil) {
         showToast(message, image: UIImage(named: "code_icon_alert"), clearTime: clearTime, completion: completion)
     }
     
-    static func showError(_ message: String, clearTime: TimeInterval = autoDismissTime,  completion: ((_ didTap: Bool)->())? = nil) {
+    public static func showError(_ message: String, clearTime: TimeInterval = autoDismissTime,  completion: ((_ didTap: Bool)->())? = nil) {
         showToast(message, image: UIImage(named: "code_icon_fail"), clearTime: clearTime, completion: completion)
     }
     
-    static func showInfo(_ message: String, clearTime: TimeInterval = autoDismissTime,  completion: ((_ didTap: Bool)->())? = nil) {
+    public static func showInfo(_ message: String, clearTime: TimeInterval = autoDismissTime,  completion: ((_ didTap: Bool)->())? = nil) {
         showToast(message, image: UIImage(named: "code_icon_alert"), clearTime: clearTime, completion: completion)
     }
     
-    static func showSuccess(_ view: UIView, message: String, clearTime: TimeInterval = autoDismissTime,  completion: ((_ didTap: Bool)->())? = nil) {
+    public static func showSuccess(_ view: UIView, message: String, clearTime: TimeInterval = autoDismissTime,  completion: ((_ didTap: Bool)->())? = nil) {
         self.showToast(view, message: message, image: UIImage(named: "icon_success_application"), clearTime: clearTime, completion: completion)
     }
     
-    static func showError(_ view: UIView, message: String, clearTime: TimeInterval = autoDismissTime,  completion: ((_ didTap: Bool)->())? = nil) {
+    public static func showError(_ view: UIView, message: String, clearTime: TimeInterval = autoDismissTime,  completion: ((_ didTap: Bool)->())? = nil) {
         self.showToast(view, message: message, image: UIImage(named: "code_icon_fail"), clearTime: clearTime, completion: completion)
     }
     
-    static func showInfo(_ view: UIView, message: String, clearTime: TimeInterval = autoDismissTime,  completion: ((_ didTap: Bool)->())? = nil) {
+    public static func showInfo(_ view: UIView, message: String, clearTime: TimeInterval = autoDismissTime,  completion: ((_ didTap: Bool)->())? = nil) {
         self.showToast(view, message: message, image: UIImage(named: "code_icon_alert"), clearTime: clearTime, completion: completion)
     }
     
-    static func wait() {
+    public static func wait() {
         self.topmostViewController?.view.makeToastActivity(.center)
     }
     
-    static func clear() {
+    public static func clear() {
         self.topmostViewController?.view.hideToast()
     }
     
-    static func clearAll() {
+    public static func clearAll() {
         self.topmostViewController?.view.hideAllToasts()
     }
     
